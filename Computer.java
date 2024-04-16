752bf4f8c9ae01479aa01aece1ff13dc8822dccf
Maksim59 <max.isayenka@gmail.com>
2024-04-16 16:59:24 -0400

    Created 2 new files

---
+++ Computer.java
@@ -0,0 +1,6 @@
+public class Computer {
+    public String gpu;
+    public String motherboard;
+    public String cpu;
+    public int numOfGB;
+}
---
+++ House.java
@@ -0,0 +1,6 @@
+public class House{
+    public String windows;
+    public int numWindows;
+    public String open;
+    public int squareFootage;
+}
