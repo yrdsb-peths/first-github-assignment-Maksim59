ff955f8b2a4070d6bac7f380d46fdb4587c76fb9
Maksim59 <max.isayenka@gmail.com>
2024-04-18 21:49:14 -0400

    Delete Dog.java

--- Dog.java
+++
@@ -1,8 +0,0 @@
-public class Dog {
-    public String breed;
-    public String firstName;
-    public int Age;
-    public String favTreat;
-    public String ownerName;
-    public int ownerPhoneNumber;
-}
